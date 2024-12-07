6b7bf84b4d76b53e83d0142bbffe30c940d19ebc
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2024-12-07 15:58:49 -0600

    Module 1 - Router

--- module-1/studio/router.py
+++ module-1/studio/router.py
@@ -1,22 +1,101 @@
+from typing_extensions import TypedDict
+from typing import Annotated
 from langchain_openai import ChatOpenAI
-from langgraph.graph import MessagesState
+from langchain_core.messages import AnyMessage
+from langgraph.graph.message import add_messages
+# from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.tools import tool
 
 
 @tool
+def plus(x: int, y: int) -> int:
+    """
+    Add x and y.
+    Args:
+        x: first int
+        y: second int
+    """
+    return x + y
+
+
+@tool
+def minus(x: int, y: int) -> int:
+    """
+    Subtract y from x.
+    Args:
+        x: first int
+        y: second int
+    """
+    return x - y
+
+
+@tool
 def multiply(a: int, b: int) -> int:
-    """Multiplies a and b."""
+    """
+    Multiplies a and b.
+    Args:
+        a: first int
+        b: second int
+    """
     return a * b
 
 
-# LLM with bound tool
+@tool
+def modulo(a: int, b: int) -> int:
+    """
+    Remainder of a division b.
+    Args:
+        a: first int
+        b: second int
+    """
+    return a % b
+
+
+@tool
+def get_small_value(a: int, b: int) -> int:
+    """
+    small value between a and b.
+    Args:
+        a: first int
+        b: second int
+    """
+    return min(a, b)
+
+
+@tool
+def get_large_value(a: int, b: int) -> int:
+    """
+    large value between a and b.
+    Args:
+        a: first int
+        b: second int
+    """
+    return max(a, b)
+
+
+tools_list = [
+    plus,
+    minus,
+    multiply,
+    modulo,
+    get_small_value,
+    get_large_value,
+]
+
+# LLM
 llm = ChatOpenAI(
     model="gpt-4o",
     temperature=0,
 )
-llm_with_tools = llm.bind_tools([multiply])
+
+# LLM with bound tool
+llm_with_tools = llm.bind_tools(tools_list)
+
+
+class MessagesState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
 
 
 # Node
@@ -26,8 +105,12 @@ def tool_calling_llm(state: MessagesState):
 
 # Build graph
 builder = StateGraph(MessagesState)
+
+# Nodes
 builder.add_node("tool_calling_llm", tool_calling_llm)
-builder.add_node("tools", ToolNode([multiply]))
+builder.add_node("tools", ToolNode(tools_list))
+
+# Edges
 builder.add_edge(START, "tool_calling_llm")
 builder.add_conditional_edges(
     "tool_calling_llm",
